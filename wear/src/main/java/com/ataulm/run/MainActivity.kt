ab928f94f62ec88e81400a48fce088c72ab4e732
Ataul Munim <ataulm@google.com>
2022-05-19 19:49:34 +0100

    Initial commit
    Created a new Wear app with blank activity (and empty phone app) with min SDK 30.

---
+++ wear/src/main/java/com/ataulm/run/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ataulm.run
+
+import android.app.Activity
+import android.os.Bundle
+import com.ataulm.run.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
