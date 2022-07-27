63469dbe6c8b93cd72a686e5e33bb5bc8f9e1afe
Ataul Munim <ataulm@google.com>
2022-07-27 22:47:47 +0100

    Add start/stop button

--- wear/src/main/java/com/ataulm/run/ui/RunScreen.kt
+++ wear/src/main/java/com/ataulm/run/ui/RunScreen.kt
@@ -15,21 +15,49 @@
  */
 package com.ataulm.run.ui
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.Text
 
 @Composable
 fun RunScreen(modifier: Modifier = Modifier) {
-    Text(
+    var buttonText by remember { mutableStateOf("Start") }
+    Column(
         modifier = modifier.fillMaxSize(),
-        text = "run",
-        textAlign = TextAlign.Center
-    )
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "00:00")
+        Chip(
+            label = {
+                Text(
+                    text = buttonText,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    textAlign = TextAlign.Center
+                )
+            },
+            onClick = {
+                // TODO: start workout
+                if (buttonText == "Start") {
+                    buttonText = "Stop"
+                } else {
+                    buttonText = "Start"
+                }
+            }
+        )
+    }
 }
 
 @WearSmallRoundPreview
